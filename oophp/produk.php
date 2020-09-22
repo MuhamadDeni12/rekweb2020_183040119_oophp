9d2adfd3e8589cb77c8cb8349172f33d76c10ad2
Muhamad Deni Akbar Alafalah <71306381+MuhamadDeni12@users.noreply.github.com>
2020-09-22 10:48:52 +0700

    Add files via upload

--- oophp/produk.php
+++ oophp/produk.php
@@ -7,7 +7,9 @@ class Produk{
 				$penulis = "penulis",
 				$penerbit = "penerbit", 
 				$harga = 0;
-
+		public function getLabel(){
+			return "$this->penulis, $this->penerbit";
+		}
 
 }
 /*$produk1 = new Produk;
@@ -25,4 +27,14 @@ $produk3->judul = "Naruto";
 $produk3->penulis = "Mashashi Kishimoto";
 $produk3->penerbit = "Shounen Jump";
 $produk3->harga = 300000;
- echo "Komik : $produk3->penulis, $produk3->penerbit";
+
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted ";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga =  250000;
+
+echo "Komik : ". $produk3->getLabel();
+echo "<br>";
+echo "Game ". $produk4->getLabel();
